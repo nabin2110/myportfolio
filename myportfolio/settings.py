7c4c8559c4073a9363ce6b0fd20628499fb8e5d1
nabin2110 <nabinthapamessifan@gmail.com>
2024-02-15 11:18:31 +0545

    all

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -82,16 +82,16 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_portfolio', 
-        'USER': 'postgres',
-        'PASSWORD': 'Nabin21lm10',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'db_portfolio', 
+#         'USER': 'postgres',
+#         'PASSWORD': 'Nabin21lm10',
+#         'HOST': '127.0.0.1', 
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT=BASE_DIR/'assets'
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL='/media/'
